00a995f42a8b6426ca54330299bd1f7d1ff89922
TG <buddhimith@gmail.com>
2018-11-16 00:07:38 +0530

    designed admin home

---
+++ Examination_System/src/application/AdminHomeController.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AdminHomeController {
+	@FXML
+	private Button Closebttn4; 
+	@FXML
+	private Button Minimizebttn4; 
+	
+	@FXML
+	private void handleClose(MouseEvent event)
+	{
+		System.exit(0);
+	}
+	
+	@FXML
+	private void handleMinimize(MouseEvent event)
+	{
+		Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		stage.setIconified(true);
+	}
+}
