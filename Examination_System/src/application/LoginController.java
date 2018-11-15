76d95f1b437f9c909c2eb550d0a0b845a0414964
TG <buddhimith@gmail.com>
2018-11-16 02:12:18 +0530

    temporary link user menus

--- Examination_System/src/application/LoginController.java
+++ Examination_System/src/application/LoginController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,12 +19,12 @@ import javafx.scene.Scene;
 
 public class LoginController  {
 	
+	
 	@FXML
-	private Button Closebttn; 
+	private Button Closebttn, Minimizebttn, TeacherRegbtn, loginbtn;
+	
 	@FXML
-	private Button Minimizebttn; 
-	@FXML
-	private Button TeacherRegbtn, loginbtn; 
+	private TextField email; 
 	
 	
 	@FXML
@@ -37,41 +40,60 @@ public class LoginController  {
 		stage.setIconified(true);
 	}
 
-	
-	
-	@FXML
-	private void promptTeacherReg(MouseEvent event) throws Exception
+	//this function contains fxml file loader. the sting value pass the  relevent fxml link
+	public void fxmlLoader(String link) throws Exception
 	{
 		Stage stage = new Stage();
-		Parent root = FXMLLoader.load(getClass().getResource("/application/TeacherRegGUI.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource(link));
 		stage.initStyle(StageStyle.UNDECORATED);
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+	}
+	
+	@FXML
+	private void promptTeacherReg(MouseEvent event) throws Exception
+	{
+		String linktoTeacherReg = "/application/TeacherRegGUI.fxml";
+		fxmlLoader(linktoTeacherReg);
+		
 	}
 	
 	@FXML
 	private void promptStudentReg(MouseEvent event) throws Exception
 	{
-		Stage stage = new Stage();
-		Parent root = FXMLLoader.load(getClass().getResource("/application/StudentRegGUI.fxml"));
-		stage.initStyle(StageStyle.UNDECORATED);
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		stage.setScene(scene);
-		stage.show();
+		String linktoStudentReg = "/application/StudentRegGUI.fxml";
+		fxmlLoader(linktoStudentReg);
+		
 	}
 	
 	@FXML
 	private void MovetoMenue(MouseEvent event) throws Exception
 	{
-		Stage stage = new Stage();
-		Parent root = FXMLLoader.load(getClass().getResource("/application/TeacherHomeGUI.fxml"));
-		stage.initStyle(StageStyle.UNDECORATED);
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		stage.setScene(scene);
-		stage.show();
+		String userid = email.getText(); //get the entered value and check the user catagory through it.
+		
+		if (userid.equals("SID") || userid.equals("sid"))
+		{
+			String linktoStudent = "/application/StudentHomeGUI.fxml";
+			fxmlLoader(linktoStudent);
+		}
+		else if (userid.equals("TID") || userid.equals("tid"))
+		{
+			String linktoTeacher = "/application/TeacherHomeGUI.fxml";
+			fxmlLoader(linktoTeacher);
+		}
+		else if (userid.equals("AID") || userid.equals("aid"))
+		{
+			String linktoAdmin = "/application/AdminHomeGUI.fxml";
+			fxmlLoader(linktoAdmin);
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Invalid login");
+		}
+	
+		
 	}
+	
 }
