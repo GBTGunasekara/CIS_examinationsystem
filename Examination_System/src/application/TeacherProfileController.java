6cfa064dd623467838201e73a745c71dbd199d0b
TG <buddhimith@gmail.com>
2018-11-20 05:01:59 +0530

    add GUIs to teacher section and link them

---
+++ Examination_System/src/application/TeacherProfileController.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class TeacherProfileController {
+	
+	@FXML
+	private Button Closebttn, Minimizebttn, ClassListbtn, PaperListbtn;
+	
+	public void fxmlLoader(String link) throws Exception
+	{
+		Stage stage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource(link));
+		stage.initStyle(StageStyle.UNDECORATED);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	@FXML
+	private void MovetoClassList(MouseEvent event) throws Exception
+	{
+		String link = "/application/ViewClassListGUI.fxml";
+		fxmlLoader(link);
+		
+	}
+	
+	@FXML
+	private void MovetoPaperList(MouseEvent event) throws Exception
+	{
+		String link = "/application/PapersListGUI.fxml";
+		fxmlLoader(link);
+		
+	}
+}
---
+++ Examination_System/src/application/ViewClassController.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ViewClassController {
+	@FXML
+	private Button Closebttn, Minimizebttn, ClassListbtn;
+	
+	public void fxmlLoader(String link) throws Exception
+	{
+		Stage stage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource(link));
+		stage.initStyle(StageStyle.UNDECORATED);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	@FXML
+	private void MovetoClassList(MouseEvent event) throws Exception
+	{
+		String link = "/application/ViewClassListGUI.fxml";
+		fxmlLoader(link);
+		
+	}
+}
